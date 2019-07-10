76e441309fd89b4f694d59e2b88a68bd78f46f3f
AHaliq <liqo12@hotmail.com>
2019-07-10 14:41:14 +0800

    removed scaryfruittree grow algo

--- src/sketch.js
+++ src/sketch.js
@@ -26,29 +26,15 @@ function draw() {
   line(RBND,BBND,BORDER,BBND);
   line(BORDER,BBND,BORDER,BORDER);
 
+  
+  trees.map((t) => t.grow());
+  trees.map((t) => t.render());
+
   petals = petals.reduce((ps,p) => {
     p.render();
     p.fall();
     return p.alpha <= 0 ? ps : [...ps, p];
   }, []);
-  
-  trees.map((t) => t.grow());
-  trees.map((t) => t.render());
-  /*let [i,j] = s.grow();
-  if(i==2 && j == 0 && SPREAD_RATE == 0) { 
-    if(tmr == 0) {
-      SPREAD_RATE = 0.25;
-      SPREAD_RATE_MAG = HALF_PI * SPREAD_RATE;
-      tmr=120;
-    }else tmr--;
-  }else if(i == 2 && j == 0 && SPREAD_RATE > 0) {
-    GROWTH_RATE *= -1;
-    SPREAD_RATE = 0;
-    SPREAD_RATE_MAG = 0;
-  }else if(i == 0 && GROWTH_RATE < 0) {
-    s = new Segment(width / 2, 400, PI, INITIAL_W, INITIAL_L);
-    GROWTH_RATE *= -1;
-  }*/
   
   /*if((i == 2 && tmr == 0) || i == 0) {
     if(i == 0) {
