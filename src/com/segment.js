4ad049ac9d53042e2c842ae1d21905aeb9279324
AHaliq <liqo12@hotmail.com>
2019-08-11 17:01:50 +0800

    refactor second time

---
+++ src/com/segment.js
@@ -0,0 +1,157 @@
+/**
+ * 
+ * @param {SegBase} s 
+ */
+export function SegBehaviour(s) {
+  behaviours = [];
+  Object.defineProperties(s, {
+    addBehaviour: {
+      value: function (b) { behaviours.push(b); }
+    },
+    step: {
+      value: function () { behaviours.map(x => x()); }
+    }
+  })
+}
+
+export function H_SegBaseW(p, x, y, a, m, r) {
+  return SegBaseW(SegBase(p, x, y, a, m), r);
+}
+
+/**
+ * 
+ * @param {SegBase} s 
+ * @param {Number} r 
+ */
+export function SegBaseW(s, r) {
+  const p = s._p;
+  let _w = Vec(p, 0, r, false);
+  let _rw, _lw = {};
+  let rchanged = true;
+  const updateW = function () {
+    if (s._tcheck() || rchanged) {
+      _w.a = s.d.a + p.HALF_PI;
+      _rw = { x: s.o.x + _w.x, y: s.o.y + _w.y };
+      _lw = { x: s.o.x - _w.x, y: s.o.y - _w.y };
+      rchanged = false;
+    }
+  }
+  Object.defineProperties(s, {
+    rw: {
+      get: function () {
+        s._updateT();
+        updateW();
+        return _rw;
+      }
+    },
+    lw: {
+      get: function () {
+        s._updateT();
+        updateW();
+        return _lw;
+      }
+    },
+    r: {
+      set: function (v) {
+        _w.m = v;
+        rchanged = true;
+      }
+    }
+  });
+  return s;
+}
+
+/**
+ * 
+ * @param {p5js} p 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Number} a 
+ * @param {Number} m 
+ */
+export function SegBase(p, x, y, a, m) {
+  let _o = Vec(p, x, y);
+  let _d = Vec(p, a, m, false);
+  let _t = Vec(p);
+  function __updateT() {
+    if (_o.check() || _d.check()) {
+      _t.x = _o.x + _d.x;
+      _t.y = _o.y + _d.y;
+    }
+  }
+  return {
+    _p: p,
+    _updateT: __updateT,
+    _tcheck: _t.check,
+    get o() { return _o; },
+    get d() { return _d; },
+    get t() {
+      __updateT();
+      return { x: _t.x, y: _t.y };
+    }
+  };
+}
+
+export function Vec(p, v1 = 0, v2 = 0, cartesian = true) {
+  let upCar = !cartesian;
+  let upAng = cartesian;
+  let upMag = cartesian;
+  let changed = true;
+  let _x = cartesian ? v1 : 0;
+  let _y = cartesian ? v2 : 0;
+  let _a = cartesian ? 0 : v1;
+  let _m = cartesian ? 0 : v2;
+  return {
+    get x() {
+      if (upCar) {
+        _x = p.cos(_a) * _m;
+        _y = p.sin(_a) * _m;
+        upCar = false;
+      }
+      return _x;
+    },
+    set x(v) {
+      _x = v;
+      upAng = upMag = changed = true;
+    },
+    get y() {
+      if (upCar) {
+        _x = p.cos(_a) * _m;
+        _y = p.sin(_a) * _m;
+        upCar = false;
+      }
+      return _y;
+    },
+    set y(v) {
+      _y = v;
+      upAng = upMag = changed = true;
+    },
+    get a() {
+      if (upAng) {
+        _a = p.atan2(_y, _x);
+        upAng = false;
+      }
+      return _a;
+    },
+    set a(v) {
+      _a = v;
+      upCar = changed = true;
+    },
+    get m() {
+      if (upMag) {
+        _m = p.sqrt(_x * _x + _y * _y);
+        upMag = false;
+      }
+      return _m;
+    },
+    set m(v) {
+      _m = v;
+      upCar = changed = true;
+    },
+    check: function () {
+      let s = changed;
+      changed = false;
+      return s;
+    }
+  };
+}
--- src/com/sketch.js
+++ src/com/sketch.js
@@ -1,30 +1,34 @@
-import PrimitiveSetup from './setups/primitive_setup';
-import BlockSetup from './setups/block_setup';
-import "../style.scss"
+import { H_SegBaseW } from './segment';
+import "../style.scss";
 
 const BORDER = 100;
-let B_WDT, B_HGT, o;
+let bounds, s;
 
 new p5((p) => {
   function calcBounds() {
-    B_WDT = p.width - BORDER * 2;
-    B_HGT = p.height - BORDER * 2;
+    bounds.o2.x = p.windowWidth - BORDER;
+    bounds.o2.y = p.windowHeight - BORDER;
   }
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    bounds = DrawBox({
+      x1: BORDER, y1: BORDER,
+      draw: (function () {
+        let s = H_SegBaseW(p, p.windowWidth * 0.5, p.windowHeight * 0.5, 0, 50, 10);
+        let t = 0;
+        return function (p) {
+          s.d.a = p.atan2(p.mouseY - p.height / 2, p.mouseX - p.width / 2);
+          s.r = p.sin(t) * 50;
+          p.ellipse(s.o.x, s.o.y, 10);
+          p.ellipse(s.t.x, s.t.y, 10);
+          p.ellipse(s.rw.x, s.rw.y, 5);
+          p.ellipse(s.lw.x, s.lw.y, 5);
+          t += p.PI / 180;
+        }
+      })()
+    });
     calcBounds();
-    switch (new URL(window.location.href).searchParams.get("type")) {
-      case 'flower':
-        //o = new FlowerSetup(BORDER);
-        break;
-      case 'primitive':
-        o = new PrimitiveSetup(p, BORDER);
-        break;
-      default: // block
-        o = new BlockSetup(p, BORDER);
-        break;
-    }
   }
 
   p.windowResized = () => {
@@ -36,9 +40,36 @@ new p5((p) => {
     p.background(220);
     p.noFill();
     p.stroke(150);
-    p.rect(BORDER, BORDER, B_WDT, B_HGT);
-    o.algo();
+    p.rectMode(p.CORNERS);
+    p.rect(bounds.o1.x, bounds.o1.y, bounds.o2.x, bounds.o2.y);
+    bounds.draw(p);
   }
 });
 
-// do flower tree
+/**
+ * 
+ * @param {*} param0 spec object:
+ * draw   draw function taking in p5js instance object as argument
+ * x1,y1,x2,y2  box spec arguments
+ */
+function DrawBox({ draw, x1 = 0, y1 = 0, x2 = 0, y2 = 0 } = {}) {
+  return {
+    draw: draw,
+    ...Box({ x1, x2, y1, y2 })
+  };
+}
+
+/**
+ * 
+ * @param {*} param0 spec object:
+ * x1 top left x coordinate;
+ * y1 top left y coordinate;
+ * x2 btm right x coordinate;
+ * y2 btm right y coordinate
+ */
+function Box({ x1 = 0, y1 = 0, x2 = 0, y2 = 0 } = {}) {
+  return {
+    o1: { x: x1, y: y1 },
+    o2: { x: x2, y: y2 }
+  }
+}
