0d6da488ff666e196443b4b5cfc8d1238dd7c5b4
AHaliq <liqo12@hotmail.com>
2019-08-13 20:39:33 +0800

    used box coordinates to render

--- src/com/sketch.js
+++ src/com/sketch.js
@@ -1,33 +1,35 @@
 import { H_SegBaseW } from './segment';
 import "../style.scss";
 
+
+//TODO DO BOUNDING BOX and folder per module
+
 const BORDER = 100;
-let bounds, s;
+let b, s;
 
 new p5((p) => {
   function calcBounds() {
-    bounds.o2.x = p.windowWidth - BORDER;
-    bounds.o2.y = p.windowHeight - BORDER;
+    b.o2.x = p.windowWidth - BORDER;
+    b.o2.y = p.windowHeight - BORDER;
   }
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    bounds = DrawBox({
-      x1: BORDER, y1: BORDER,
-      draw: (function () {
-        let s = H_SegBaseW(p, p.windowWidth * 0.5, p.windowHeight * 0.5, 0, 50, 10);
-        let t = 0;
-        return function (p) {
-          s.d.a = p.atan2(p.mouseY - p.height / 2, p.mouseX - p.width / 2);
-          s.r = p.sin(t) * 50;
-          p.ellipse(s.o.x, s.o.y, 10);
-          p.ellipse(s.t.x, s.t.y, 10);
-          p.ellipse(s.rw.x, s.rw.y, 5);
-          p.ellipse(s.lw.x, s.lw.y, 5);
-          t += p.PI / 180;
-        }
-      })()
-    });
+    b = Box({ x1: BORDER, y1: BORDER });
+    b.draw = (function () {
+      let s = H_SegBaseW(p, 0, 0, 0, 50, 10);
+      let t = 0;
+      return function (p) {
+        p.translate(b.o1.x + b.width * 0.5, b.o1.y + b.height * 0.5);
+        s.d.a = p.atan2(p.mouseY - b.o1.y - b.height / 2, p.mouseX - b.o1.x - b.width / 2);
+        s.r = p.sin(t) * 50;
+        p.ellipse(s.o.x, s.o.y, 10);
+        p.ellipse(s.t.x, s.t.y, 10);
+        p.ellipse(s.rw.x, s.rw.y, 5);
+        p.ellipse(s.lw.x, s.lw.y, 5);
+        t += p.PI / 180;
+      }
+    })();
     calcBounds();
   }
 
@@ -41,35 +43,21 @@ new p5((p) => {
     p.noFill();
     p.stroke(150);
     p.rectMode(p.CORNERS);
-    p.rect(bounds.o1.x, bounds.o1.y, bounds.o2.x, bounds.o2.y);
-    bounds.draw(p);
+    p.rect(b.o1.x, b.o1.y, b.o2.x, b.o2.y);
+    b.draw(p);
   }
 });
 
-/**
- * 
- * @param {*} param0 spec object:
- * draw   draw function taking in p5js instance object as argument
- * x1,y1,x2,y2  box spec arguments
- */
-function DrawBox({ draw, x1 = 0, y1 = 0, x2 = 0, y2 = 0 } = {}) {
+function Box({ draw, x1 = 0, y1 = 0, x2 = 0, y2 = 0 } = {}) {
+  let _o1 = { x: x1, y: y1 };
+  let _o2 = { x: x2, y: y2 };
   return {
     draw: draw,
-    ...Box({ x1, x2, y1, y2 })
-  };
-}
-
-/**
- * 
- * @param {*} param0 spec object:
- * x1 top left x coordinate;
- * y1 top left y coordinate;
- * x2 btm right x coordinate;
- * y2 btm right y coordinate
- */
-function Box({ x1 = 0, y1 = 0, x2 = 0, y2 = 0 } = {}) {
-  return {
-    o1: { x: x1, y: y1 },
-    o2: { x: x2, y: y2 }
+    o1: _o1,
+    o2: _o2,
+    get width() { return _o2.x - _o1.x },
+    get height() { return _o2.y - _o1.y },
+    set width(v) { _o2.x = v + _o1.x; },
+    set height(v) { _o2.y = v + _o1.y; }
   }
 }
